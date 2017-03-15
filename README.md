0c412d265d4feb818a1a380aa5829e5ff3f61c11
adal.io <hello@adal.io>
2017-03-14 22:47:54 -0700

    Update export function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pwa-icon-generator
+
+Generate and resize icons from a base image.
+
+## Usage
+
+```` bash
+./icons.sh [icon.png]
+````
--- icons.sh
+++ icons.sh
@@ -1,4 +1,5 @@
 array=(
+    '512::icon-512x512.png'
     '256::icon-256x256.png'
     '192::icon-192x192.png'
     '152::icon152x152.png'
@@ -7,10 +8,17 @@ array=(
     '32::icon-32x32.png'
 )
 
+INPUT=$1
+INPUT=${INPUT:-icon.png}
 
+if [ ! -f "$INPUT" ]; then
+	echo $INPUT does not exist
+	exit
+fi
 
 for index in "${array[@]}" ; do
     KEY="${index%%::*}"
     VALUE="${index##*::}"
-    sips -Z $KEY icon.png $VALUE
+    cp $INPUT $VALUE
+    sips -Z $KEY $VALUE
 done
